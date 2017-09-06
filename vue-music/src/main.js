0e2ccfcf07455bd1c8bd237d222abd86fd3d05b1
christinazy <1739008292@qq.com>
2017-09-06 23:49:53 +0800

    vue-loader

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -1,11 +1,11 @@
-import 'babel-polifll'
+// import 'babel-polifll'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastclick from './fastclick'
+import fastclick from 'fastclick'
 // 去除移动端300ms延迟
-fastclick.attach(document.body);
-Vue.config.productionTip = false
+fastclick.attach(document.body)
+
 
 /* eslint-disable no-new */
 new Vue({
