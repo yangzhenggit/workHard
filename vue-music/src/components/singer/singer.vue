ea3c11434150427a13a7aeb6f3836322236aa6e8
christinazy <1739008292@qq.com>
2017-09-30 22:51:55 +0800

    singer

--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
-    歌手
+  <div class="singer" ref="singer">
+    <list-view :data="singers" ref="list"></list-view>
   </div>
 </template>
 <script>
-  export default{}
-</script>
-<style scoped rel="stylesheet/stylus">
+  export default{
+      data() {
+          return {
+              singers: []
+          }
+      },
+      created() {
+          this._getSingerList();
+      },
+      methods: {
+          _getSingerList() {
 
+          }
+      }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
