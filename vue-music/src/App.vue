4b693ec0e117e3674012fb18e5de01a88f18fe66
christinazy <1739008292@qq.com>
2018-01-14 22:54:44 +0800

    play-vuex

--- vue-music/src/App.vue
+++ vue-music/src/App.vue
@@ -3,16 +3,19 @@
     <m-header></m-header>
     <tab></tab>
     <router-view></router-view>
+    <player></player>
   </div>
 </template>
 
 <script>
   import MHeader from 'components/m-header/m-header'
   import Tab from 'components/tab/tab'
+  import player from 'components/player/player'
   export default {
     components: {
       MHeader,
-      Tab
+      Tab,
+      player
     }
   }
 </script>
