29a30d8b3e3d69357eaf4da5290e92d195bd0bee
yangzheng <Christina@08011>
2018-01-17 18:15:52 +0800

    play

--- vue-music/src/components/player/player.vue
+++ vue-music/src/components/player/player.vue
@@ -1,46 +1,55 @@
 <template>
     <div class="player" v-show="playList.length > 0">
-      <div class="normal-player" v-show="fullScreen">
-        <div class="background">
-          <img width="100%" height="100%" :src="currentSong.image" alt="">
-        </div>
-        <div class="top">
-          <div class="back" @click="back">
-            <i class="icon-back"></i>
+      <transition name="normal"
+                  @enter="enter"
+                  @after-enter="afterEnter"
+                  @leave="leave"
+                  @after-leave="afterLeave"
+      >
+        <div class="normal-player" v-show="fullScreen">
+          <div class="background">
+            <img width="100%" height="100%" :src="currentSong.image" alt="">
           </div>
-          <div class="title" v-html="currentSong.name"></div>
-          <div class="subtitle" v-html="currentSong.singer"></div>
-        </div>
-        <div class="middle">
-          <div class="middle-l">
-            <div class="cd-wrapper">
-              <div class="cd">
-                <img class="image" :src="currentSong.image" alt="">
+          <div class="top">
+            <div class="back" @click="back">
+              <i class="icon-back"></i>
+            </div>
+            <div class="title" v-html="currentSong.name"></div>
+            <div class="subtitle" v-html="currentSong.singer"></div>
+          </div>
+          <div class="middle">
+            <div class="middle-l">
+              <div class="cd-wrapper" ref="cdWrapper">
+                <div class="cd">
+                  <img class="image" :src="currentSong.image" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="bottom">
+            <div class="operators">
+              <div class="icon i-left">
+                <i class="icon-sequence"></i>
+              </div>
+              <div class="icon i-left">
+                <i class="icon-prev"></i>
+              </div>
+              <div class="icon i-center">
+                <i class="icon-play"></i>
+              </div>
+              <div class="icon i-right">
+                <div class="icon icon-next"></div>
+              </div>
+              <div class="icon i-right">
+                <i class="icon icon-not-favorite"></i>
               </div>
             </div>
           </div>
         </div>
-        <div class="bottom">
-          <div class="operators">
-            <div class="icon i-left">
-              <i class="icon-sequence"></i>
-            </div>
-            <div class="icon i-left">
-              <i class="icon-prev"></i>
-            </div>
-            <div class="icon i-center">
-              <i class="icon-play"></i>
-            </div>
-            <div class="icon i-right">
-              <div class="icon icon-next"></div>
-            </div>
-            <div class="icon i-right">
-              <i class="icon icon-not-favorite"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="mini-player" v-show="!fullScreen" @click="open">
+      </transition>
+      <!--miniPlayer-->
+      <transition name="mini">
+        <div class="mini-player" v-show="!fullScreen" @click="open">
           <div class="icon">
             <img width="40" height="40" :src="currentSong.image" alt="">
           </div>
@@ -52,11 +61,17 @@
           <div class="control">
             <i class="icon-playlist"></i>
           </div>
-      </div>
+        </div>
+      </transition>
     </div>
 </template>
 <script>
     import {mapGetters, mapMutations} from 'vuex'
+    import animations from 'create-keyframe-animation'
+    import {prefixStyle} from 'common/js/dom'
+
+    const transform = prefixStyle('transform')
+    const transitionDuration = prefixStyle('transitionDuration')
     export default{
         data(){
             return{
@@ -80,6 +95,59 @@
           },
           open() {
             this.setFullScreen(true)
+          },
+          enter(el, done) {
+            const {x, y, scale} = this._getPosAndScale()
+            let animation = {
+                0: {
+                    transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+                },
+                60: {
+                  transform: `translate3d(0, 0, 0) scale(1.1})`
+                },
+                100: {
+                  transform: `translate3d(0, 0, 0) scale(1)`
+                },
+            }
+            animations.registerAnimation({
+              name: 'move',
+              animation,
+              presets: {
+                duration: 400,
+                easing: 'linear'
+              }
+            })
+
+            animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+          },
+          afterEnter() {
+            animations.unregisterAnimation('move')
+            this.$refs.cdWrapper.style.animation = ''
+          },
+          leave(el, done) {
+            this.$refs.cdWrapper.style.transition = 'all .4s'
+            const {x, y, scale} = this._getPosAndScale();
+            this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+            this.$refs.cdWrapper.addEventListener('transitionend', done)
+          },
+          afterLeave() {
+            this.$refs.cdWrapper.style.transition = ''
+            this.$refs.cdWrapper.style[transform] = ''
+          },
+          _getPosAndScale() {
+            const targetWidth = 40
+            const paddingLeft = 40
+            const paddingTop = 80
+            const paddingBottom = 30
+            const width = window.innerWidth * 0.8
+            const scale = targetWidth / width
+            const x = - (window.innerWidth/2 - paddingLeft)
+            const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+            return {
+                x,
+                y,
+                scale
+            }
           },
           ...mapMutations({
             setFullScreen: 'SET_FULL_SCREEN'
